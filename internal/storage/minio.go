edb944676257d1ec3c536c7cb5f55dd48f329118
random <1687352409@qq.com>
2025-07-25 22:23:47 +0800

    success run minio and health heart

--- internal/storage/minio.go
+++ internal/storage/minio.go
@@ -23,7 +23,7 @@ func NewMinioClient(cfg MinioConfig) (*minio.Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	bucketName := "cloudStor"
+	bucketName := "cloudstor"
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
